ecb336854c14dfac49fd6e5729b82a6f8fd58ef9
cindy-kilonzo <cindy.kilonzo@ashesi.edu.gh>
2025-02-13 21:02:17 +0000

    Update

--- src/DonorHomepageController.java
+++ src/DonorHomepageController.java
@@ -1,9 +1,35 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DonorHomepageController {
     @FXML
     private Button logoutButton;
+
+    @FXML
+    private Button findOrgButton;
+
+    @FXML
+    private Button viewDonations;
+
+    @FXML
+    public void handleBackButton(ActionEvent event) throws IOException {
+        Parent signupparent = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene signUpScene = new Scene(signupparent);
+
+        // Get stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(signUpScene);
+        window.show();
+    }
 
 
 }
